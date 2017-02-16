997176803bd95784e76ced4feecff972b7fbca61
Adrian31 <adrian_carabetta@hotmail.com>
2017-02-16 13:11:57 +0800

    Formatted JS properly and added a pause when player wins or loses

--- js/app.js
+++ js/app.js
@@ -29,15 +29,11 @@ Enemy.prototype.update = function(dt) {
     // which will ensure the game runs at the same speed for
     // all computers.
 
-    if(this.x < 510){
-      this.x += dt * this.speed;
-    }else {
-      this.x = -100;
-    };
-
-
-
-
+    if (this.x < 510) {
+        this.x += dt * this.speed;
+    } else {
+        this.x = -100;
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -51,55 +47,56 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 var Player = function() {
-  this.x = 201;
-  this.y = 375;
+    this.x = 201;
+    this.y = 375;
 
-  this.score = 0;
-  //Players score
+    this.score = 0;
+    //Players score
 
-  this.lives = 3;
-  //Default lives of the player
+    this.lives = 3;
+    //Default lives of the player
 
-  this.sprite = 'images/char-boy.png';
+    this.sprite = 'images/char-boy.png';
 };
 
 var resetPlayer = function() {
-  player.x = 201;
-  player.y = 375;
+    player.x = 201;
+    player.y = 375;
 };
 
 
 Player.prototype.render = function() {
-  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 Player.prototype.update = function(dt) {
-  this.drawText();
-  this.increaseScore();
-  this.enemyCollision();
+    this.drawText();
+    this.increaseScore();
+    this.enemyCollision();
 
 
-    if(player.score >= 5 && player.y === 0) {
+    if (player.score >= 5 && player.y === 0) {
         $('#game-won').show();
         $('.won').click(function() {
             $('#game-won').hide();
             document.location.reload();
         });
+        setTimeout(gameLoop, 1000 / 30);
     }
 };
 
 Player.prototype.drawText = function() {
-  ctx.clearRect(0, 0, 120, 20);
-  ctx.clearRect(400, 0, 100, 20);
-  ctx.font = "20px Verdana";
-  ctx.fillStyle = "black";
-  ctx.fillText("Score: " + this.score, 8, 20);
-  ctx.fillText("Lives: " + this.lives, 400, 20);
+    ctx.clearRect(0, 0, 120, 20);
+    ctx.clearRect(400, 0, 100, 20);
+    ctx.font = "20px Verdana";
+    ctx.fillStyle = "black";
+    ctx.fillText("Score: " + this.score, 8, 20);
+    ctx.fillText("Lives: " + this.lives, 400, 20);
 };
 
 
 Player.prototype.increaseScore = function() {
-    if(hasReachedWater) {
+    if (hasReachedWater) {
         this.score++;
         setTimeout(resetPlayer);
         hasReachedWater = false;
@@ -110,71 +107,67 @@ Player.prototype.enemyCollision = function() {
     var bug = checkCollisions(allEnemies);
     //if collision detected, reduce a player life.
     //Game over if all lives lost.
-    if(bug) {
-        if(this.lives !== 0) {
+    if (bug) {
+        if (this.lives !== 0) {
             this.lives--;
             resetPlayer();
-        }
-        else {
+        } else {
             $('#game-over').show();
             $('.lost').click(function() {
                 $('#game-over').hide();
                 document.location.reload();
             });
+            setTimeout(gameLoop, 1000 / 30);
         }
     }
 };
 
 var checkCollisions = function(targetArray) {
-    for(var i = 0; i < targetArray.length; i++) {
-        if(player.x < targetArray[i].x + 50 &&
+    for (var i = 0; i < targetArray.length; i++) {
+        if (player.x < targetArray[i].x + 50 &&
             player.x + 50 > targetArray[i].x &&
             player.y < targetArray[i].y + 40 &&
             player.y + 40 > targetArray[i].y) {
-                return targetArray[i];
+            return targetArray[i];
         }
     }
 };
 
-  //switchcase that gets the arrow direction desired and checks for wall collision
-  Player.prototype.handleInput = function(key){
-    switch(key) {
+//switchcase that gets the arrow direction desired and checks for wall collision
+Player.prototype.handleInput = function(key) {
+    switch (key) {
         case 'left':
-        if(this.x - tileWidth < 0){
-            this.x = 0;
-        }
-        else {
-            this.x -= tileWidth;
-        }
-        break;
+            if (this.x - tileWidth < 0) {
+                this.x = 0;
+            } else {
+                this.x -= tileWidth;
+            }
+            break;
 
         case 'right':
-        if(this.x + tileWidth >= rightEdge){
-            this.x = 404;
-        }
-        else {
-            this.x += tileWidth;
-        }
-        break;
+            if (this.x + tileWidth >= rightEdge) {
+                this.x = 404;
+            } else {
+                this.x += tileWidth;
+            }
+            break;
 
         case 'up':
-        if(this.y - tileHeight < 0){
-            hasReachedWater = true;
-            this.y = 0;
-        }
-        else {
-            this.y -= tileHeight;
-        }
-        break;
+            if (this.y - tileHeight < 0) {
+                hasReachedWater = true;
+                this.y = 0;
+            } else {
+                this.y -= tileHeight;
+            }
+            break;
 
         case 'down':
-        if(this.y + tileHeight >= bottomEdge){
-            this.y = 375;
-        }
-        else {
-            this.y += tileHeight;
-        }
-        break;
+            if (this.y + tileHeight >= bottomEdge) {
+                this.y = 375;
+            } else {
+                this.y += tileHeight;
+            }
+            break;
     }
 };
 
@@ -182,19 +175,26 @@ var checkCollisions = function(targetArray) {
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-var allEnemies = [  new Enemy(-100, 60, 500), new Enemy(-100, 140, 300), new Enemy(-100, 225, 225) ];
-  /*new Enemy(0, 60, 500),
-  new Enemy(0, 140, 300),
-  new Enemy(0, 225, 225) */
-  //if(hasReachedWater){
-  for(var i = 0; i < 1; i++){
-    var enemyX = -100/* * (6 - 1)) + 1) * 101 */;
-    var enemyY = (Math.floor(Math.random() * 6 )+ 100) /* * (5 - 1)) + 1) * 50 */;
-    var enemy = new Enemy(enemyX, enemyY);
-    allEnemies.push(enemy);
+var allEnemies = [new Enemy(-100, 60, 500), new Enemy(-100, 140, 300), new Enemy(-100, 225, 225)];
 
-  //};
+//not entirely sure if this is the best way of doing this.... But it had the desired effect
+
+//spawn 3 extra enemies
+var spawnEnemy = function() {
+    for (var i = 0; i < 3; i++) {
+        var enemyX = -400;
+        var enemyY = 225;
+        if (i == 1) {
+            enemyY = 140;
+        } else if (i == 2) {
+            enemyY = 60;
+        };
+
+        var enemy = new Enemy(enemyX, enemyY);
+        allEnemies.push(enemy);
+    };
 };
+spawnEnemy();
 
 var player = new Player();
 
@@ -202,10 +202,10 @@ var player = new Player();
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
+        37: "left",
+        38: "up",
+        39: "right",
+        40: "down"
     };
 
     player.handleInput(allowedKeys[e.keyCode]);
